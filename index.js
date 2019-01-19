112fc6f26c6431d8b99a85b0bf5f575c1eafabfc
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-19 16:30:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, { [key]: value });
 }
 
+
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
   recipes[key] = value;
   return recipes;
 }
 
+
 function deleteFromObjectByKey(recipes, key) {
   var newRecipes = Object.assign ({}, recipes);
   delete newRecipes[key];
 }
+
+function 
